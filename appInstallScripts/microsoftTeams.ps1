5597521011c9733df2a60b3074c6413c0809ccd7
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2021-09-24 15:39:01 +0100

    Update and rename teams.ps1 to microsoftTeams.ps1

--- appInstallScripts/microsoftTeams.ps1
+++ appInstallScripts/microsoftTeams.ps1
@@ -1,4 +1,4 @@
-Write-Output 'Teams Start'
+Write-Output 'MicrosoftTeams Start'
 $BuildDir = 'c:\CustomizerArtifacts'
 if (-not(Test-Path $BuildDir)) {
     New-Item  -ItemType Directory $BuildDir
@@ -17,4 +17,4 @@ New-item $teamsRegKey | Out-Null
 New-ItemProperty -Path $teamsRegKey -Name 'IsWVDEnvironment' -Value 1 -PropertyType DWORD | Out-Null
 Start-Process -FilePath msiexec.exe -Argument "/i $outFile /quiet /norestart ALLUSER=1 ALLUSERS=1" -Wait
 Remove-Item $outFile
-Write-Output 'Teams Installed'
+Write-Output 'MicrosoftTeams Installed'
