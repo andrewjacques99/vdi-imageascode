04839ace4cf88e5c6693bce004a812a5e7e8e5b3
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2021-10-22 19:37:47 +0100

    Add files via upload

---
+++ appInstallScripts/NotepadPlusPlus.ps1
@@ -0,0 +1,17 @@
+﻿Write-Output 'NotepadPlusPlus Start'
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)) {
+    New-Item  -ItemType Directory $BuildDir
+}
+$allVersions = Find-EvergreenApp -Name NotepadPlusPlus | Get-EvergreenApp
+$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
+$myVersion = $allOnVersion | Where-Object { $_.Architecture -eq 'x64'}
+$fileName = split-path $myVersion.uri -Leaf
+$outFile = join-path 'c:\CustomizerArtifacts' $fileName
+if (-not(Test-Path $outFile)) {
+    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+}
+Start-Process -FilePath $outFile -Argument "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
+Remove-Item $outFile
+Write-Output 'NotepadPlusPlus Installed'
