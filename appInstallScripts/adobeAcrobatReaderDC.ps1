c3254ff25890abfcc8a5921d4dada4835f3bcfc8
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2021-12-01 18:28:50 +0000

    Update adobeAcrobatReaderDC.ps1

--- appInstallScripts/adobeAcrobatReaderDC.ps1
+++ appInstallScripts/adobeAcrobatReaderDC.ps1
@@ -1,17 +1,23 @@
 Write-Output 'AdobeAcrobatReaderDC Start'
+#If c:\CustomizerArtifacts directory doesnt exist then create it
 $BuildDir = 'c:\CustomizerArtifacts'
 if (-not(Test-Path $BuildDir)) {
     New-Item  -ItemType Directory $BuildDir
 }
+#Get all MS OneDrive installers
 $allVersions = Find-EvergreenApp -Name AdobeAcrobatReaderDC | Get-EvergreenApp
-$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
-$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
-$allOnArchitecture = $allOnVersion | Where-Object { $_.Architecture -eq 'x64'}
-$myVersion = $allOnArchitecture | Where-Object { $_.Language -eq 'English (UK)'}
-$fileName = split-path $myVersion.uri -Leaf
+#Filter 'English (UK)' Language installers
+$ukInstallers = $allVersions | Where-Object { $_.Language -eq 'English (UK)'}
+#Filter 'x64' architecture installers
+$x64Installers = $ukInstallers | Where-Object { $_.Architecture -eq 'x64'}
+#Filter the most recent installer version
+$mostRecent = $x64Installers | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+#Get the most recent installer version
+$myInstaller = $x64Installers | Where-Object { $_.version -eq $mostRecent }
+$fileName = split-path $myInstaller.uri -Leaf
 $outFile = join-path 'c:\CustomizerArtifacts' $fileName
 if (-not(Test-Path $outFile)) {
-    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+    Invoke-WebRequest $myInstaller.uri -OutFile $outFile
 }
 Start-Process -FilePath $outFile -Argument "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
 Remove-Item $outFile
