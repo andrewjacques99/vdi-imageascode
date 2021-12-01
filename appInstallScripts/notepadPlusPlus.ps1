98072289b951d968c1100f601a744ea4f280d4df
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2021-12-01 18:44:57 +0000

    Update notepadPlusPlus.ps1

--- appInstallScripts/notepadPlusPlus.ps1
+++ appInstallScripts/notepadPlusPlus.ps1
@@ -1,17 +1,22 @@
-﻿Write-Output 'NotepadPlusPlus Start'
+Write-Output 'NotepadPlusPlus Start'
+#If c:\CustomizerArtifacts directory doesnt exist then create it
 $BuildDir = 'c:\CustomizerArtifacts'
 if (-not(Test-Path $BuildDir)) {
     New-Item  -ItemType Directory $BuildDir
 }
+#Get all Notepad++ installers
 $allVersions = Find-EvergreenApp -Name NotepadPlusPlus | Get-EvergreenApp
-$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
-$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
-$myVersion = $allOnVersion | Where-Object { $_.Architecture -eq 'x64'}
-$fileName = split-path $myVersion.uri -Leaf
+#Filter 'x64' architecture installers
+$x64Installers = $allVersions | Where-Object { $_.Architecture -eq 'x64'}
+#Filter the most recent installer version
+$mostRecent = $x64Installers | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+#Get the most recent installer version
+$myInstaller = $x64Installers | Where-Object { $_.version -eq $mostRecent }
+$fileName = split-path $myInstaller.uri -Leaf
 $outFile = join-path 'c:\CustomizerArtifacts' $fileName
 if (-not(Test-Path $outFile)) {
-    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+    Invoke-WebRequest $myInstaller.uri -OutFile $outFile
 }
-Start-Process -FilePath $outFile -Argument "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
+Start-Process -FilePath $outFile -Argument "/S" -Wait
 Remove-Item $outFile
 Write-Output 'NotepadPlusPlus Installed'
