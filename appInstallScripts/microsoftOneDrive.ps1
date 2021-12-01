0dcd2add57946e28d2e56aa5185c30aef5d65aad
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2021-12-01 18:02:02 +0000

    Update microsoftOneDrive.ps1

--- appInstallScripts/microsoftOneDrive.ps1
+++ appInstallScripts/microsoftOneDrive.ps1
@@ -1,17 +1,28 @@
 Write-Output 'MicrosoftOneDrive Start'
+#If c:\CustomizerArtifacts directory doesnt exist then create it
 $BuildDir = 'c:\CustomizerArtifacts'
 if (-not(Test-Path $BuildDir)) {
     New-Item  -ItemType Directory $BuildDir
 }
-$allVersions = Find-EvergreenApp -Name MicrosoftOneDrive | Get-EvergreenApp
-$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
-$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
-$myVersion = $allOnVersion | Where-Object { $_.Architecture -eq 'AMD64'}
-$fileName = split-path $myVersion.uri -Leaf
+#Get all MS OneDrive installers
+$allInstallers = Find-EvergreenApp -Name MicrosoftOneDrive | Get-EvergreenApp
+#Filter 'Production' ring installers
+$prodInstallers = $allInstallers | Where-Object { $_.Ring -eq 'Production'}
+#Filter 'exe' type installers
+$exeInstallers = $prodInstallers | Where-Object { $_.Type -eq 'exe'}
+#Filter 'AMD64' architecture installers
+$AMD64Installers = $prodInstallers | Where-Object { $_.Architecture -eq 'AMD64'}
+#Filter the most recent installer version
+$mostRecent = $AMD64Installers | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+#Get the most recent installer version
+$myInstaller = $AMD64Installers | Where-Object { $_.version -eq $mostRecent }
+$fileName = split-path $myInstaller.uri -Leaf
 $outFile = join-path 'c:\CustomizerArtifacts' $fileName
 if (-not(Test-Path $outFile)) {
-    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+    Invoke-WebRequest $myInstaller.uri -OutFile $outFile
 }
-Start-Process -FilePath $outFile -Argument "/allusers" -Wait
+#Install OneDrive
+Start-Process -FilePath $outFile -Argument "/allusers /SILENT" -Wait
+#Remove install file
 Remove-Item $outFile
 Write-Output 'MicrosoftOneDrive Installed'
