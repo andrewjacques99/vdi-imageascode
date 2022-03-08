e87206acb032c01a41385e5707501668388f4ea5
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2022-03-08 09:30:55 +0000

    Update microsoftPowerBi.ps1

--- appInstallScripts/microsoftPowerBi.ps1
+++ appInstallScripts/microsoftPowerBi.ps1
@@ -2,6 +2,6 @@ echo 008-PowerBi-Start >> C:\\buildActions\\buildActionsOutput.txt
 $outfile = "C:\TEMP\PBIDesktopSetup_x64.exe"
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://statlasautomation002.blob.core.windows.net/installers/power_bi/PBIDesktopSetup_x64.exe", $outfile)
-Start-Process -FilePath $outFile -Argument "/silent" 
+Start-Process -FilePath $outFile -Argument "-silent -norestart ACCEPT_EULA=1" -wait
 Remove-Item $outFile
 echo 008-PowerBi-Installed >> C:\\buildActions\\buildActionsOutput.txt
