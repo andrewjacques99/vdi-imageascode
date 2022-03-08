e730e9b065bbbbd8271499d2f477bcf543f2ef95
andrewjacques99 <65302208+andrewjacques99@users.noreply.github.com>
2022-03-08 09:14:12 +0000

    Update n-ableViewer.ps1

--- appInstallScripts/n-ableViewer.ps1
+++ appInstallScripts/n-ableViewer.ps1
@@ -1,5 +1,8 @@
+echo 016-N-Able-Viewer-Start >> C:\\buildActions\\buildActionsOutput.txt
 $outfile = "C:\TEMP\N-AbleViewerSetup.exe"
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://statlasautomation002.blob.core.windows.net/installers/n-able/MSPA4NCentralViewerInstall-7.00.26-20210708.exe", $outfile)
-Start-Process -FilePath $outFile -Argument "/S /R" 
-Start-Sleep -Seconds 60
+Start-Process -FilePath $outFile -Argument "/silent" 
+Start-Sleep -Seconds 80
+Remove-Item $outFile
+echo 016-N-Able-Viewer-Installed >> C:\\buildActions\\buildActionsOutput.txt
